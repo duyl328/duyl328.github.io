6118a80ac469109b4134195162f4b0c8c4e6f08c
charlatans <yllyou333@163.com>
2025-01-18 00:48:37 +0800

    更新任务

---
+++ docs/.vitepress/theme/components/NotesRedirect.vue
@@ -0,0 +1,17 @@
+<template>
+</template>
+<script setup lang="ts">
+import { useRouter } from 'vitepress'
+
+import { createSideBarZH } from '../utils/createSideBar'
+
+const router = useRouter()
+
+const sideBar = createSideBarZH()
+
+const prefix = '/notes/'
+const firstItemLink = sideBar[prefix][0].items[0].link
+
+router.go(firstItemLink)
+</script>
+<style scoped></style>
